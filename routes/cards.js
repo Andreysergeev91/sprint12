c72f0d1446e871c212a6b3186434139eded29e0c
Andrey Sergeev <andreyser07@gmail.com>
2020-05-05 17:00:28 +0700

    Add "fs.promise.readFile" and "path" in cards.js
    Modules "fs.promise.readFile" and "path" were added in cards.js.
    Callback function of cardsRouter was chanded.

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,21 @@
 const cardsRouter = require('express').Router();
 
-const cards = require('../data/cards');
+const fsPromises = require('fs').promises;
+
+const path = require('path');
+
+const cardsPath = path.join('data', 'cards.json');
+
 
 cardsRouter.get('/cards', (req, res) => {
-  res.send(cards);
+  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    });
 });
 
 module.exports = cardsRouter;
